d09865317b184517aaf52ba9885c912be224228d
Tri Pham <K21003770@kcl.ac.uk>
2022-10-13 00:52:09 +0100

    fixed comments

--- employee.py
+++ employee.py
@@ -67,32 +67,24 @@ class Hourly_Employee(Employee):
         output += f' Their total pay is {self.get_pay()}'
         return output
 
-# def main():•••••
-#
-#
-#     # Billie works on a monthly salary of 4000.  Their total pay is 4000.
-#     billie = Salary_Employee('Billie',4000)
-#
-#     # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
-#     charlie = Hourly_Employee('Charlie',100,25)
-#
-#     # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
-#     renee = Salary_Employee('Renee',3000,4,200)
-#
-#     # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
-#     jan = Hourly_Employee('Jan',150,25,3,220)
-#
-#     # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
-#     robbie = Salary_Employee('Robbie',2000,0,0,1500)
-#
-#     # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
-#     ariel = Hourly_Employee('Ariel',120,30,0,0,600)
-#
-#     print(billie.__str__())
-#     print(charlie.__str__())
-#     print(renee.__str__())
-#     print(jan.__str__())
-#     print(robbie.__str__())
-#     print(ariel.__str__())
+
+
+     # Billie works on a monthly salary of 4000.  Their total pay is 4000.
+     billie = Salary_Employee('Billie',4000)
+
+     # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
+     charlie = Hourly_Employee('Charlie',100,25)
+
+     # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
+     renee = Salary_Employee('Renee',3000,4,200)
+
+     # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
+     jan = Hourly_Employee('Jan',150,25,3,220)
+
+     # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
+     robbie = Salary_Employee('Robbie',2000,0,0,1500)
+
+     # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
+     ariel = Hourly_Employee('Ariel',120,30,0,0,600)
 
 
